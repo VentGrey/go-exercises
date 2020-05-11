ff11bab5eaec16805dcbaf4d1e5469e2a223494d
VentGrey <omarpurataf@gmail.com>
2020-05-10 23:17:36 -0500

    Finished cone exercise

--- basics/cone.go
+++ basics/cone.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var rad float64;
 	var alt float64;
 	var gene float64;
 
+	fmt.Println("Ingrese el radio del cono");
+	fmt.Scan(&rad);
 
+	fmt.Println("Ingrese la altura del cono");
+	fmt.Scan(&alt);
+
+	fmt.Println("Ingrese la generatriz del cono");
+	fmt.Scan(&gene);
+
+	AB := math.Pi * (rad * rad);
+	AL := math.Pi * rad * gene;
+
+	fmt.Println("AB:", AB);
+	fmt.Println("AL:", AL);
+	fmt.Println("AT:", AB + AL);
+	fmt.Println("Vol:", (1.0/3.0) * AB * alt);
 }
