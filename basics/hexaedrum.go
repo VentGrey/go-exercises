72eb105e5684cc1a449047038b5c8f265332b1ce
VentGrey <omarpurataf@gmail.com>
2020-05-11 17:00:31 -0500

    Finished hexaedrum exercise

--- basics/hexaedrum.go
+++ basics/hexaedrum.go
@@ -6,5 +6,13 @@ import (
 )
 
 func main() {
+	var l float64;
 
+	fmt.Println("Ingrese el lado del hexaedro");
+	fmt.Scan(&l);
+
+	fmt.Println("AB:", math.Pow(l, 2));
+	fmt.Println("AL:", 4 * math.Pow(l, 2));
+	fmt.Println("AT:", 6 * math.Pow(l, 2));
+	fmt.Println("Vol:", math.Pow(l, 3));
 }
